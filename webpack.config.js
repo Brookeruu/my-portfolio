a4a2deaa76fa1687b4520f131a97bfff4a3ead3d
Brooke <perrysbrooke@gmail.com>
2019-02-15 08:02:41 -0800

    setup project environment

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'Farmers Market',
+      title: 'My Portfolio',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
